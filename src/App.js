031a701a3bcf785c2faf94a0c963ed806c1cf1f0
Mac Connolly <mac@macconnolly.com>
2019-04-14 09:36:00 -0600

    final wp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MovieHeader from './components/movieHeader';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+        <div>
+          <MovieHeader />
+          {this.props.children}
+        </div>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom' // react-router v4
+import { ConnectedRouter } from 'connected-react-router'
+
+import configureStore, { history } from './store/configureStore';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import PrivateRoute from './container/privateRoute'
+import LoginPage from './components/loginPage';
+import RegisterPage from './components/registerPage';
+import CreateMovieContainer from './container/createMovieContainer';
+import MovieHeader from "./components/movieHeader";
+import MovieOverview from './container/movieOverview';
+import MovieForm from './components/movieForm';
+import MovieList from "./components/movieList";
+
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+
+                <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */ }
+                    <BrowserRouter>
+                        <MovieHeader />
+                        <Switch>
+                            <Route path='/' exact component={LoginPage} />
+                            <Route path='/login' component={LoginPage} />
+                            <Route path='/register' component={RegisterPage} />
+                            <PrivateRoute path="/movies" component={MovieOverview} />
+                            <PrivateRoute path='/movie/create' exact component={CreateMovieContainer}/>
+                            <PrivateRoute path='/new/movie' exact component={MovieForm}/>
+                        </Switch>
+                    </BrowserRouter>
+        </ConnectedRouter>
+
+    </Provider>,
+    document.getElementById('root')
+)
